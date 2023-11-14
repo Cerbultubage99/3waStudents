aa38349e5fc02dbb6ba1c77703f6a5dd2a573021
maxencem <maxencebosquain@gmail.com>
2023-11-14 11:55:59 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import chalk from 'chalk'
+
+const tabName = ["Sandra", "nathan", "samuel", "lou"]
+
+console.log( chalk.blue  ( tabName[0] ) )
+console.log( chalk.yellow( tabName[1] ) )
+console.log( chalk.red   ( tabName[2] ) )
+console.log( chalk.green ( tabName[3] ) )
